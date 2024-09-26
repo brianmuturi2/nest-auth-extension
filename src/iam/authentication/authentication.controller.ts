b51ddbd28d2429f1d136853a1878accd11deb05c
brian <brianmuturi2@gmail.com>
2024-09-26 13:44:58 +0300

    feat(auth): implement jwt

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up/sign-up.dto';
 import { SignInDto } from './dto/sign-in/sign-in.dto';
+import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -16,7 +17,16 @@ export class AuthenticationController {
 
     @HttpCode(HttpStatus.OK)
     @Post('sign-in')
-    signIn(@Body() signInDto: SignInDto) {
+    async signIn(
+        @Res({passthrough: true}) response: Response,
+        @Body() signInDto: SignInDto
+    ) {
         return this.authenticationService.signIn(signInDto);
+        const accessToken = await this.authenticationService.signIn(signInDto);
+        response.cookie('accessToken', accessToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: true
+        })
     }
 }
