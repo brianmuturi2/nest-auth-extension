5a5b3c35b035155ea6040e68877cdd7e54908a5f
brian <brianmuturi2@gmail.com>
2024-09-26 11:30:49 +0300

    feat(iam): implement hashing1

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'postgres',
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+    IamModule
   ],
   controllers: [AppController],
   providers: [AppService],
